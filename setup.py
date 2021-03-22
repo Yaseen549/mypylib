d2ea7268fc0f719e8170dbd9b6b67d961a1c48d9
Yaseen <43935210+Yaseen549@users.noreply.github.com>
2021-03-22 21:20:54 +0530

    Add files via upload

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
 	long_description = fh.read()
 
 setuptools.setup(
-	name="pywebcracker",
+	name="pymywebweb",
 	version="0.0.1",
 	author="Mohamed Yaseen Sheriff S",
 	author_email="fantasticyaseenshariff@gmail.com",
